a5342c9881e4b8b5ab4134d34cdaee422f92d056
ramah1528 <rmahkillll@gmail.com>
2025-06-26 17:04:09 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "bot": {
+    "owners": ["1236449427192025218"],
+    
+    "botID": "1387506126437744772",
+    "GuildId": "1387402501132914810",
+    "ClientId": "1387506126437744772",
+    "serverinvte": "https://discord.gg/code",
+    "clientSECRET": "MFmZupYHg3GbzA-zM77HOo0_N6S4m3p1",
+    "callbackURL": "https://b70d781f-a67a-4785-b9b1-6d17684cc01e-00-s6azdomannbs.riker.replit.dev/login",
+    "inviteBotUrl": "https://discord.com/oauth2/authorize?client_id=1387506126437744772&permissions=1099511678977&integration_type=0&scope=bot",
+    "TheLinkVerfy": "https://discord.com/oauth2/authorize?client_id=1387506126437744772&response_type=code&redirect_uri=https%3A%2F%2Fb70d781f-a67a-4785-b9b1-6d17684cc01e-00-s6azdomannbs.riker.replit.dev%2Flogin&scope=gdm.join+guilds.join+guilds+identify",
+    "prefix": "$",
+    "ceatogry": "1387404934839799869",
+    "TOKEN": "MTM4NzUwNjEyNjQzNzc0NDc3Mg.GazWdC.R6ryA5DG3HPdmbdZXVdevAE2COVRBfgUh_IQOM",
+    "TraId": "1236449427192025218",
+    "price": "500000",
+    "channelId": "1387404020364218418",
+    "roleId": "1387435568568336486",
+    "LineIce": "https://cdn.discordapp.com/attachments/1387473129311436984/1387474227946590331/54_20250515150043.png.jpg?ex=685d7991&is=685c2811&hm=51b2ddf582e273084bb3712c5805f4c7854e09a0a0d840732f4239ada4629c49&",
+    "taxchannels": "",
+    "coinprice": "500000"
+  },
+  "Log": {
+    "LogChannelOwners": "1387406307937746995"
+  },
+  "website": {
+    "PORT": "22069"
+  }
+};
